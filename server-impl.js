d57a2b0609f36a54af63207d6676d71a15b042bc
craig-rainbird <craig.cochran@rainbird.ai>
2017-04-26 10:35:46 +0100

    triple-webservice changes to support YOL-413 - datasource headers. (#7)

--- server-impl.js
+++ server-impl.js
@@ -76,6 +76,22 @@ app.get('/:key/query/:subject/:relationship', function(req, res) {
     processQuery(req.params.key, req.params.subject, req.params.relationship, data, req, res);
 });
 
+app.get('/:key/securequery/:subject/:relationship', function(req, res) {
+
+    var authorization = req.header('Authorization');
+
+    if ((typeof authorization == 'undefined')) {
+        res.status(401).send({'status': 'ERROR', 'message': 'Missing Authorization header!'});
+    } else {
+        var data = {
+            'query': req.query,
+            'body': {}
+        };
+
+        processQuery(req.params.key, req.params.subject, req.params.relationship, data, req, res);
+    }
+});
+
 app.post('/:key/query/:subject/:relationship', function(req, res) {
 
     var data = {
